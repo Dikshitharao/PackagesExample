2631a9cd46a1faf6e3d5b21988536fae9381cb6d
Dikshitha Rao <dikshitharao48798@gmail.com>
2021-06-13 15:38:10 +0530

    Add files via upload

---
+++ PackagesExample/src/com/blogging/add/Main.java
@@ -0,0 +1,14 @@
+package com.blogging.add;
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 6;
+		int c = a+b;
+		System.out.println("Main class in add package"+c);
+
+	}
+
+}
---
+++ PackagesExample/src/com/blogging/sub/Main.java
@@ -0,0 +1,14 @@
+package com.blogging.sub;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 6;
+		int c = a-b;
+		System.out.println("Main class in sub package"+c);
+
+	}
+
+}
